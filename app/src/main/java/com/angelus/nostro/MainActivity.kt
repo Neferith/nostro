818099079ad3c7818fe8353f78fbde1258c97348
Neferith <vivien.bernard@gmail.com>
2025-02-09 20:21:01 +0100

    feat(): First version of navigation

--- app/src/main/java/com/angelus/nostro/MainActivity.kt
+++ app/src/main/java/com/angelus/nostro/MainActivity.kt
@@ -4,14 +4,57 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.angelus.nostro.ui.theme.NostroTheme
+
+// Screen.kt
+sealed class Screen(val route: String) {
+    object Main : Screen("main_screen")
+    object Detail : Screen("detail_screen")
+}
+
+@Composable
+fun NavigationStack() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = Screen.Main.route) {
+        composable(route = Screen.Main.route) {
+            MainScreen(navController = navController)
+        }
+        composable(
+            route = Screen.Detail.route + "?text={text}",
+            arguments = listOf(
+                navArgument("text") {
+                    type = NavType.StringType
+                    nullable = true
+                }
+            )
+        ) {
+            DetailScreen(text = it.arguments?.getString("text"))
+        }
+    }
+}
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,16 +62,21 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NostroTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    NavigationStack()
                 }
             }
+
         }
     }
+
 }
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
@@ -42,6 +90,40 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     NostroTheme {
-        Greeting("Android")
+        Greeting("Anaelle Bernard")
+    }
+}
+
+@Composable
+fun MainScreen(navController: NavController) {
+    val text = remember {
+        mutableStateOf("")
+    }
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        OutlinedTextField(value = text.value, onValueChange = {
+            text.value = it
+        })
+
+        Button(onClick = {
+            navController.navigate(route = Screen.Detail.route + "?text=${text.value}")
+        }) {
+            Text("Next Screen")
+        }
+    }
+}
+
+@Composable
+fun DetailScreen(text: String?) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text("Detail Screen")
+        Text("$text")
     }
 }
