92d481f9a461eb32b40b9214db47382dcb0787d7
Neferith <vivien.bernard@gmail.com>
2025-02-09 20:24:54 +0100

    feat(): Update lib version

--- app/src/main/java/com/angelus/nostro/MainActivity.kt
+++ app/src/main/java/com/angelus/nostro/MainActivity.kt
@@ -7,11 +7,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
