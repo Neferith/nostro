6c2f7a622261a22b557936f42205f5e88b9babdd
Vivien BERNARD <vivien.bernard@gmail.com>
2025-02-11 21:00:55 +0100

    Feature/integrate navigation (#2)
    * feat(): First version of navigation, first coordiator version, not perfect

--- app/src/main/java/com/angelus/nostro/MainActivity.kt
+++ app/src/main/java/com/angelus/nostro/MainActivity.kt
@@ -4,14 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.angelus.nostro.coordinator.AppCoordinator
 import com.angelus.nostro.ui.theme.NostroTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,16 +29,23 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NostroTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navController = rememberNavController()
+                    val appCoordinator = remember { AppCoordinator(navController) }
+                    appCoordinator.Start()
                 }
             }
+
         }
     }
+
 }
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
@@ -42,6 +59,46 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     NostroTheme {
-        Greeting("Android")
+        Greeting("Anaelle Bernard")
+    }
+}
+
+
+interface MainNavigator {
+    fun goToDetail(value: String)
+}
+
+@Composable
+fun MainScreen(navigator: MainNavigator) {
+    val text = remember {
+        mutableStateOf("")
+    }
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        OutlinedTextField(value = text.value, onValueChange = {
+            text.value = it
+        })
+
+        Button(onClick = {
+            navigator.goToDetail(text.value)
+            //navController.navigate(route = Screen.Detail.route + "?text=${text.value}")
+        }) {
+            Text("Next Screen")
+        }
+    }
+}
+
+@Composable
+fun DetailScreen(text: String?) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text("Detail Screen")
+        Text("$text")
     }
 }
